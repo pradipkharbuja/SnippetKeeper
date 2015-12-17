fc975fe022b74fddce31cd42e40c6b369e434ab6
Ezimet Yusup <ezimet.yusup@otkur.biz>
2015-12-17 01:25:54 -0600

    bug report bug fixed.

--- src/org/snippetkeeper/controller/BugReportController.java
+++ src/org/snippetkeeper/controller/BugReportController.java
@@ -4,12 +4,16 @@ import java.io.File;
 import java.util.List;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.apache.log4j.Logger;
 import org.snippetkeeper.domain.Bug;
+import org.snippetkeeper.domain.User;
 import org.snippetkeeper.service.BugService;
+import org.snippetkeeper.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -31,24 +35,30 @@ public class BugReportController {
 	@Autowired
 	private BugService bugService;
 	
+	@Autowired
+	private UserService userService;
+
+	
 	@RequestMapping(value="/add",method=RequestMethod.GET)
 	public String addBugForm(@ModelAttribute("bug")Bug bug){
 		return "bug_report";
 	}
 	
 	@RequestMapping(value="/add",method=RequestMethod.POST)
-	public String addBug(@Valid @ModelAttribute("bug")Bug bug,BindingResult bindingResult,RedirectAttributes redirect){
+	public String addBug(@Valid @ModelAttribute("bug")Bug bug,BindingResult bindingResult,RedirectAttributes redirect,HttpServletRequest request){
 		if(bindingResult.hasErrors())
 		return "bug_report";
 		
-		
-		
+		Object userId = request.getSession().getAttribute("userId");
+		Long uId = (Long) userId;
+		User user = userService.getUser(uId);
+		bug.setUser(user);
 		MultipartFile bugImage = bug.getFile();
 		String rootDirectory = context.getRealPath("/");
 				
 			if (bugImage!=null && !bugImage.isEmpty()) {
 		       try {
-		    	   String fileName = bug.getEmail()+System.currentTimeMillis();
+		    	   String fileName = ""+uId+System.currentTimeMillis();
 		    	   String imagePath =rootDirectory+"/resource/images/"+ fileName+".png";
 		    	   bug.setImagePath(fileName);
 		      	bugImage.transferTo(new File(imagePath));
@@ -80,6 +90,7 @@ public class BugReportController {
 			model.addAttribute("bug", bug);
 		return "bug_detail";
 	}
+	
 	
 	@RequestMapping(value="/list",method=RequestMethod.GET)
 	public String bugList(Model model){
